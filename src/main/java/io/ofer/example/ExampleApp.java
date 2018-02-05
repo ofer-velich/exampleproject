1d1e88ff3a29528c74569a21f100220535863141
ofer-velich <ofer@logz.io>
2018-02-05 13:19:11 +0200

    gitignore

--- src/main/java/io/ofer/example/ExampleApp.java
+++ src/main/java/io/ofer/example/ExampleApp.java
@@ -4,11 +4,13 @@ import java.util.concurrent.TimeUnit;
 
 public class ExampleApp {
     public static void main(String[] args) throws InterruptedException {
-        System.out.print("Example App Run");
+        System.out.println("Example App Run");
 
         for (int i = 0; i < 5; i++) {
-            System.out.print("Example App Log");
+            System.out.println("Example App Log");
             TimeUnit.SECONDS.sleep(60);
         }
+
+        System.out.println("Example App Done");
     }
 }
