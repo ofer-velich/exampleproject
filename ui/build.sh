766d8160a8e4594ccc5179104391210655ebc1df
ofer-velich <ofer@logz.io>
2018-02-12 21:54:56 +0200

    test

--- ui/build.sh
+++ ui/build.sh
@@ -2,7 +2,7 @@
 
 set -ev
 
-echo docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+echo "docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD"
 cd ui
     docker run --rm -v $PWD:/data digitallyseamless/nodejs-bower-grunt npm install
     docker run --rm -v $PWD:/data digitallyseamless/nodejs-bower-grunt bower install
