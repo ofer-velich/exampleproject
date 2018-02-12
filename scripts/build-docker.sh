730d1e09fddef3c2d2d78ae4914b113986fcb8c8
ofer-velich <ofer@logz.io>
2018-02-12 21:42:59 +0200

    test

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -4,5 +4,6 @@ set -ev
 
 GIT_COMMIT=`git rev-parse HEAD`
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-docker build -t ofervelich/test:$GIT_COMMIT .
-docker push ofervelich/test:$GIT_COMMIT
+docker build -t ofervelich/test:$TRAVIS_COMMIT .
+docker push ofervelich/test:$TRAVIS_COMMIT
+OMMIT
--- ui/build.sh
+++ ui/build.sh
@@ -2,16 +2,8 @@
 
 set -ev
 
-GIT_COMMIT=`git rev-parse HEAD`
-
 cd ui
     docker run --rm -v $PWD:/data digitallyseamless/nodejs-bower-grunt npm install
     docker run --rm -v $PWD:/data digitallyseamless/nodejs-bower-grunt bower install
     docker run --rm -v $PWD:/data digitallyseamless/nodejs-bower-grunt grunt build --force
 cd -
-
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-
-
-#docker build -t ofervelich/test:$GIT_COMMIT .
-#docker push ofervelich/test:$GIT_COMMIT
