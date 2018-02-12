5261c95a4dd4841760c513b128497c7440146354
ofer-velich <ofer@logz.io>
2018-02-12 13:34:13 +0200

    building UI

---
+++ scripts/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -ev
+
+GIT_COMMIT=`git rev-parse HEAD`
+
+cd ../ui
+echo `pwd`
+docker run --rm -v $PWD:/data digitallyseamless/nodejs-bower-grunt npm install
+docker run --rm -v $PWD:/data digitallyseamless/nodejs-bower-grunt bower install
+docker run --rm -v $PWD:/data digitallyseamless/nodejs-bower-grunt grunt build --force
+cd -
+#
+#docker build -t logzio/apollo:$GIT_COMMIT .
+#docker push logzio/apollo:$GIT_COMMIT
