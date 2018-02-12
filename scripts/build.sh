c34f00df153aabf81c0b0bd2af097ce1055ad914
ofer-velich <ofer@logz.io>
2018-02-12 13:57:02 +0200

    test

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,14 +2,16 @@
 
 set -ev
 
-GIT_COMMIT=`git rev-parse HEAD`
+pwd
+#GIT_COMMIT=`git rev-parse HEAD`
 
-cd ../ui
-echo `pwd`
-docker run --rm -v $PWD:/data digitallyseamless/nodejs-bower-grunt npm install
-docker run --rm -v $PWD:/data digitallyseamless/nodejs-bower-grunt bower install
-docker run --rm -v $PWD:/data digitallyseamless/nodejs-bower-grunt grunt build --force
-cd -
+#cd ../ui
+#echo `pwd`
+#docker run --rm -v $PWD:/data digitallyseamless/nodejs-bower-grunt npm install
+#docker run --rm -v $PWD:/data digitallyseamless/nodejs-bower-grunt bower install
+#docker run --rm -v $PWD:/data digitallyseamless/nodejs-bower-grunt grunt build --force
+#cd -
+
 #
 #docker build -t logzio/apollo:$GIT_COMMIT .
 #docker push logzio/apollo:$GIT_COMMIT
